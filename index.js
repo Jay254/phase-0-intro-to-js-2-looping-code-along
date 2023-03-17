51847fe69b13b60fa8ea3a1b5e322015aa78c60e
Jay254 <jyegon504@gmail.com>
2023-03-17 17:05:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// for (let age =30; age <40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+function writeCards(arr, event){
+    let newArr = [];
+     for(let i = 0; i<arr.length; i++){
+         newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+     }
+     return newArr;
+}
+
+function countDown(int){
+   let i = 0;
+   while(i<=int){
+    console.log(int);
+    int--;
+
+   }
+}
